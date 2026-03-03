d0e33b7e767f77e0b532d32642392cdd6653a446
Hugo Arcos <hugo.arcos@paramq.com>
2026-03-03 00:47:54 -0600

    commit 7

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,8 @@ const { Pool } = require('pg');
 //Hacemos la conexion de la base de datos a Pool
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    SSL: {
-        rejectsUnauthorized: false
+    ssl: {
+        rejectUnauthorized: false
     },
     //configuracion recomendable para evitar problemas de rendimiento y estabilidad
     max: 5
