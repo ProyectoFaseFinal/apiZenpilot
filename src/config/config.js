70fb1b8e840a81300095dfe5ed4729d8ebe00434
Hugo Arcos <hugo.arcos@paramq.com>
2026-03-03 00:27:25 -0600

    commit 5

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,15 @@
 const {DB_USER, DB_DATABASE, DB_PASSWORD, DB_HOST, DB_PORT } =  require('dotenv').config();
+const { rejects } = require('assert');
 const { Pool } = require('pg');
 
 //Hacemos la conexion de la base de datos a Pool
 const pool = new Pool({
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    
+    connectionString: process.env.DATABASE_URL,
+    SSL: {
+        rejectsUnauthorized: false
+    },
     //configuracion recomendable para evitar problemas de rendimiento y estabilidad
-    max: 20
+    max: 5
 });
 
 //verificamos errores de pool que puedan surgir
