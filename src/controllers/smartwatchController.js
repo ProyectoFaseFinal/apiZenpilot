7232e0a8d0157abae0941d67c0b921a0665c6980
Hugo Arcos <hugo.arcos@paramq.com>
2026-03-12 17:17:05 -0600

    actualizaci{on de smartwatch

--- src/controllers/smartwatchController.js
+++ src/controllers/smartwatchController.js
@@ -2,16 +2,21 @@ const pool = require('../config/config.js');
 
 //controlador para obtener toda la información médica
 module.exports.getSmartwatch = async (req, res) => {
-    //consulta a la base de datos para obtener toda la información médica
-    const query = 'SELECT * FROM smartwatch';
-    const result = await pool.query(query);
-    //enviar la respuesta al cliente
-    if (result.rows.length > 0) {
-        console.log('Smartwatch obtenida correctamente', result.rows);
-        return res.status(200).json(result.rows);
-    } else {
-        console.log('No se encontró información de la Smartwatch', result.rows);
-        return res.status(404).json({ message: 'No se encontró información de la Smartwatch' });
+    try {
+        //consulta a la base de datos para obtener toda la información médica
+        const query = 'SELECT * FROM smartwatch';
+        const result = await pool.query(query);
+        //enviar la respuesta al cliente
+        if (result.rows.length > 0) {
+            console.log('Smartwatch obtenida correctamente', result.rows);
+            return res.status(200).json(result.rows);
+        } else {
+            console.log('No se encontró información de la Smartwatch', result.rows);
+            return res.status(404).json({ message: 'No se encontró información de la Smartwatch' });
+        }
+    } catch (error) {
+        console.error('Error al obtener la información de la Smartwatch: ', error);
+        return res.status(500).json({ message: 'Error con la base de datos' });
     }
 }
 
@@ -20,6 +25,11 @@ module.exports.postSmartwatch = async (req, res) => {
     try {
         //Obtener el cuerpo de la petición
         const {idUser, frecuencia_cardiaca, velocidad_promedio} = req.body;
+        //VALIDAD BODY
+        if (!idUser || !frecuencia_cardiaca || !velocidad_promedio) {
+            console.log('Faltan campos obligatorios en el cuerpo de la petición', req.body);
+            return res.status(400).json({ message: 'Faltan campos obligatorios en el cuerpo de la petición' });
+        }
         //consulta SQL
         const query = 'INSERT INTO smartwatch (idUser, frecuencia_cardiaca, velocidad_promedio) VALUES ($1, $2, $3)';
         const values = [idUser, frecuencia_cardiaca, velocidad_promedio];
